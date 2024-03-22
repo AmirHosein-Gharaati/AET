1e8b1b3bf8ea625cfb5b1eed1f91998e08a8b5c6
amirhosein <amirhosein@cyberdesign.se>
2024-03-23 00:48:52 +0330

    feat: init project

---
+++ src/main/java/com/example/aet/AetApplication.java
@@ -0,0 +1,13 @@
+package com.example.aet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AetApplication.class, args);
+	}
+
+}
