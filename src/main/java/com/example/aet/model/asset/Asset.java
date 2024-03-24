c9604c2a80a8d472571bd5b5d5b7840a7e74d266
amirhosein <amirhosein@cyberdesign.se>
2024-03-24 12:46:30 +0330

    fix: remove createdDate annotation and use LocalDateTime

--- src/main/java/com/example/aet/model/asset/Asset.java
+++ src/main/java/com/example/aet/model/asset/Asset.java
@@ -4,7 +4,6 @@ import com.example.aet.model.asset.dto.AssetRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -20,11 +19,11 @@ public class Asset {
     private String userId;
     private String name;
     private String currency;
-    @CreatedDate
     private LocalDateTime createdAt;
 
     public Asset(AssetRequest request) {
         this.name = request.name();
         this.currency = request.currency();
+        this.createdAt = LocalDateTime.now();
     }
 }
