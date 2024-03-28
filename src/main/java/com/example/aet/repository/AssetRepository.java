7be1b76f16e162264a08a5c64cf3db155a15c553
amirhosein <amirhosein@cyberdesign.se>
2024-03-28 13:55:14 +0330

    fix(asset): remove project fields in finding all assets

--- src/main/java/com/example/aet/repository/AssetRepository.java
+++ src/main/java/com/example/aet/repository/AssetRepository.java
@@ -2,16 +2,12 @@ package com.example.aet.repository;
 
 import com.example.aet.model.asset.Asset;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface AssetRepository extends MongoRepository<Asset, String> {
-
-    @Query(fields = "{id: 1, name: 1, currency: 1, totalAmount: 1}")
     List<Asset> findByUserId(String userid);
-
     Optional<Asset> findByIdAndUserId(String id, String userId);
     boolean existsByIdAndUserId(String id, String userId);
 }
