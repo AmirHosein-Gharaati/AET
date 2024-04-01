feb4018890b6df9c648b91e092383fa0207b5543
amirhosein <amirhosein@cyberdesign.se>
2024-04-01 23:18:23 +0330

    fix: use RestController for ImageController

--- src/main/java/com/example/aet/controller/ImageController.java
+++ src/main/java/com/example/aet/controller/ImageController.java
@@ -11,11 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@RestController
 @RequestMapping("/images")
 @RequiredArgsConstructor
 @Slf4j
